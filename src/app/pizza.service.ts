89b912acc93483eebb6f9bdc1b63d064ad94b97f
Maëlic Pépin <pmaelic@outlook.fr>
2025-02-10 16:38:28 +0100

    Utilisation des environnements

--- src/app/pizza.service.ts
+++ src/app/pizza.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../environments/environment.development';
 import { PizzaModel } from './models/pizza.model';
 
 @Injectable({
@@ -10,7 +11,6 @@ export class PizzaService {
   private readonly http = inject(HttpClient);
 
   pizzasList(): Observable<PizzaModel[]> {
-    return this.http.get<PizzaModel[]>("http://localhost:8080/api/products");
+    return this.http.get<PizzaModel[]>(`${environment.apiURL}/products`);
   }
-  
 }
