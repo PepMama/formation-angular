fc447547527bcc5570faf7f8cb1d8796df0a374c
Maëlic Pépin <pmaelic@outlook.fr>
2025-02-10 16:57:21 +0100

    Utilisation des routes pour naviguer entre home et la liste des pizzas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { PizzasComponent } from './pizzas/pizzas.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
+    { path: '', component: HomeComponent },
     { path: 'pizzas', component: PizzasComponent }
 ];
