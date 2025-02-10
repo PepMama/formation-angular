bdb596da8599b4e3e166cc51422304b0225c3217
Maëlic Pépin <pmaelic@outlook.fr>
2025-02-10 16:48:30 +0100

    Utilisation des routeurs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,13 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from "./menu/menu.component";
-import { PizzasComponent } from "./pizzas/pizzas.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MenuComponent, PizzasComponent],
+  imports: [RouterOutlet, MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  readonly title = signal("Kiosquitos");
-
-  // constructor(){
-  //   this.title.set("TOTO");
-  // }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { PizzasComponent } from './pizzas/pizzas.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'pizzas', component: PizzasComponent }
+];
